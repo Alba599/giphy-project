60bd633bdd2dd2d672dd0886068661ab262bf815
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-04-12 11:38:44 -0500

    alert message

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 $(document).ready(function () {
-    $('#submit').click(getData);
-    function getData(event) {
-        event.preventDefault();
-        var input = encodeURIComponent($('#search').val().trim())
-        //console.log(input)
-        var xhr = $.get(`https://api.giphy.com/v1/gifs/search?api_key=yI34f6CbEH1WmJlu2CactEwQfto6LTgc&q=${input}&limit=20&offset=0&rating=G&lang=en`)
-        xhr.done(function (response) {
-            var gifs = response.data
-            console.log(response)
-            $('#textBox').empty();
-            for (gif of gifs) {
-                $('#textBox').append(`<a href='${gif.images.downsized_large.url}' target="_blank"><img src='${gif.images.downsized_large.url}'></a>`);
-            };
-        });
-        xhr.fail(function () {
-            alert('An error occurred, please try again later');
-        });
-    };
+
+    $('#submit').click(getGIF);
+
+    function getGIF() {
+        $('#textBox').empty();
+        var results = 0;
+        var inputKeywords = $('#search').val().trim();
+        if (inputKeywords == null || inputKeywords == "") {
+            alert("Please enter a valid keywords to search!");
+        }
+        else {
+            getData(event);
+        }
+
+        function getData(event) {
+            event.preventDefault();
+            var input = encodeURIComponent($('#search').val().trim())
+            var xhr = $.get(`https://api.giphy.com/v1/gifs/search?api_key=yI34f6CbEH1WmJlu2CactEwQfto6LTgc&q=${input}&limit=20&offset=0&rating=G&lang=en`)
+            xhr.done(function (response) {
+                var gifs = response.data
+                $('#textBox').empty();
+                for (gif of gifs) {
+                    $('#textBox').append(`<a href='${gif.images.downsized_large.url}' target="_blank"><img src='${gif.images.downsized_large.url}'></a>`);
+                };
+            });
+            xhr.fail(function () {
+                alert('An error occurred, please try again later');
+            });
+        };
+
+
+    }
 });
+
