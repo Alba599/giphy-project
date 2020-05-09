65ba2086f3df6aaae3b6ebec568456da65f01197
Alba599 <57162043+Alba599@users.noreply.github.com>
2020-05-09 13:03:13 -0500

    Updates

---
+++ precache-manifest.e1d9fb41383ebd4ab1a1d71d1ebf6af5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "361659e324b2df9a415347922f07c21b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a9046d03355661fd17e5",
+    "url": "/static/css/main.eb56269b.chunk.css"
+  },
+  {
+    "revision": "a56106d3cf220b213474",
+    "url": "/static/js/2.0f5a81f8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f5a81f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9046d03355661fd17e5",
+    "url": "/static/js/main.f50d56ac.chunk.js"
+  },
+  {
+    "revision": "1a66ad65d2ede2197746",
+    "url": "/static/js/runtime-main.ae7d880f.js"
+  }
+]);
